21828f4337c92e37314e106034455c98290b6c1e
vbianchi.dev <vitor.biansil@gmail.com>
2026-08-20 08:14:32 -0300

    fix: correcao nos exports

--- projects/design-system/src/public-api.ts
+++ projects/design-system/src/public-api.ts
@@ -2,5 +2,5 @@
  * Public API Surface of design-system
  */
 
-export * from './lib/components/bui-typewriter/bui-typewriter';
-export * from './lib/pipe/fill-null-pipe';
+export { BuiTypewriter } from './lib/components/bui-typewriter/bui-typewriter';
+export { FillNullPipe } from './lib/pipe/fill-null-pipe';
